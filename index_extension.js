9cabc2cfce9595b81280640c971e8a9a3143b6b9
maryhalv <39344430+maryhalv@users.noreply.github.com>
2019-02-17 01:29:52 -0500

    emergency contact number

--- index_extension.js
+++ index_extension.js
@@ -16,7 +16,9 @@ $(document).ready(function () {
                                   startRemember();
                                  }
             
-        }); 
+        });
+            
+
             
         });
 
@@ -24,20 +26,21 @@ function startRemember(){
     
     console.log('reached');
     
-    $('.questions').append("<p>What is your first name?</p>");
+    $('body').append("<p>What is your first name?</p>");
     $('body').append("<div class = 'form-group'><form><input id='first' type = 'text'></input></form></div>");
     
     $('body').append("<p>What is your last name?</p>");
     $('body').append("<div class = 'form-group'><form><input id='last' type = 'text'></input></form></div>");
     
-    $('body').append("<p>What is your birthdate?</p>");
-    $('body').append("<div class = 'form-group'><form><input id='last' type = 'text'></input></form></div>");
+   $('body').append("<p>What is your Emergency Contact number?</p>");
+    $('body').append("<div class = 'form-group'><form><input id='num' type = 'text'></input></form></div>");
     
-        $('body').append("<p>Who is your Emergency Contact?</p>");
-    $('body').append("<div class = 'form-group'><form><input id='last' type = 'text'></input></form></div>");
     
     // "firstname": document.getElementById('first').value,
     //"lastname": document.getElementById('last').value
+        
+    
+ //  $('body').append('<p>When were you born?: <input type="text" id="datepicker"></p>');    
     
     $('body').append("<button id ='myButton'>Enter Information</button>");
     
@@ -50,44 +53,24 @@ function assignFields() {
     
     console.log('reached');
     
+    if(document.getElementById('first').value !== null ) {
      chrome.storage.local.set({firstname: document.getElementById('first').value}, function() {
           console.log('Value is set to ' + document.getElementById('first').value);
         });
+    }
      
+     if(document.getElementById('last').value !== null ) {
      chrome.storage.local.set({lastname: document.getElementById('last').value}, function() {
           console.log('Value is set to ' + document.getElementById('last').value);
         });
-      
-        window.location.href = 'newtab.html';
-       /*$.ajax({
-            url: 'https://maryhalv.github.io/pearlhacks2019/manifest.json',
-  type: 'GET',
-  dataType: 'json',
-   success: (response) => {
-    console.log('reached');
-    console.log(response.person.firstname);
-    console.log(response.person.lastname);
-               let first = response.person.firstname;
-               let last = response.person.lastname;
-                 
-                 if (first.localeCompare('empty') === 0 && last.localeCompare('empty') === 0) {
- $.ajax({
-    url: 'https://maryhalv.github.io/pearlhacks2019/manifest.json',
-    type: 'PUT',
-  dataType: 'json',
-  data: {
-  "person" : {
-   "firstname": document.getElementById('first').value,
-    "lastname": document.getElementById('last').value
-  }
-  },
-   success: () => {
-   console.log('success');
-  
-   }
+     }
+     
+     if(document.getElementById('num').value !== null ) {
+     chrome.storage.local.set({num: document.getElementById('num').value}, function() {
+          console.log('Value is set to ' + document.getElementById('num').value);
         });
-                 }
-               }
-            
-       }); */
+     }
+    
+        window.location.href = 'newtab.html';
+       
 }
