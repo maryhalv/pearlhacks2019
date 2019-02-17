d82db1009af6bd0bfcd322bd6efa391f22663286
jessicamlynch <43474943+jessicamlynch@users.noreply.github.com>
2019-02-17 09:28:55 -0500

    Added emergency contact and did additional formatting

--- newtab.js
+++ newtab.js
@@ -8,38 +8,38 @@ function getNames() {
     var place;
 
     var today = new Date();
-    today.toString();
+    today.toDateString();
     $('#age').empty();
     $('#age').append('Today is ' + today);
       chrome.storage.local.get(['firstname'], function(result) {
          first = result;
           console.log('Value currently is ' + result.firstname);
         });
-      
+
       chrome.storage.local.get(['lastname'], function(result) {
             $('#name').empty();
             $('#name').append("Hello, " + first.firstname + " " + result.lastname);
           //document.getElementById('name').value = "Hello " +  first + " " + result;
           console.log('Value currently is ' + result.lastname);
         });
-      
+
        chrome.storage.local.get(['place'], function(result) {
-        
+
           //  $('.info').append("<h4 id = 'place'>Feeling scared? Call " + result.num + "</h4>");
           place = result.place;
           console.log('Was born in ' + result.place);
         });
-       
+
         chrome.storage.local.get(['year'], function(result) {
-        
+
             $('.info').append("<h4 id = 'year'>You were born in " + place + " in the year " + result.year +  ".</h4>");
           console.log('Was born in the year ' + result.year);
         });
-      
+
       chrome.storage.local.get(['num'], function(result) {
-        
+
             $('.info').append("<h5 id = 'number'>Feeling scared? Call " + result.num + "</h5>");
           console.log('Emergency contact number is ' + result.num);
         });
-      
+
 }
